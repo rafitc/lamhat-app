2cbf1212b279eb43e0d5b5ed49eeec193bb6022b
Rafi <rafirasheed.tc99@gmail.com>
2024-09-12 07:24:16 +0530

    Header and login page

---
+++ src/app/user/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header"; // Adjust the path as necessary
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Page content goes here */}
+        <h1>This is</h1>
+      </main>
+    </div>
+  );
+}
